3ce058fe14ffb12d6646d5bb4fc4a9da087084b6
Merryl Canlas <merrylcanlas-paymongo@Merryls-MacBook-Pro.local>
2021-07-19 20:40:56 +0800

    updated for heroku

--- db/schema.rb
+++ db/schema.rb
@@ -12,10 +12,13 @@
 
 ActiveRecord::Schema.define(version: 2021_07_18_171211) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.text "description"
-    t.integer "user_id", null: false
+    t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_categories_on_user_id"
@@ -25,7 +28,7 @@ ActiveRecord::Schema.define(version: 2021_07_18_171211) do
     t.string "name"
     t.text "description"
     t.string "status"
-    t.integer "category_id", null: false
+    t.bigint "category_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.date "duedate"
